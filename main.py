8377c14ad8f67bd8620503ccb5222d10cce812ac
IvanZavialov <vaneek596@gmail.com>
2022-11-26 17:50:38 +0300

    Final commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def main():
-    print('first print')
-    print('second print')
-    print('third print')
-    print('fourth print')
+    print('1 print')
+    print('2 print')
+    print('3 print')
+    print('4 print')
     print('5 print')
 if __name__ == '__main__':
     main()
